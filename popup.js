a054ab69cfbf95cc476b483d1cbc4f288fd21cb1
Michael Hirt <hirtm14@gmail.com>
2024-12-03 18:59:08 -0800

    change styling, change title of extension

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
 document.getElementById("saveColor").addEventListener("click", () => {
     const color = document.getElementById("arrowColor").value;
-  
+
     // Save the color to Chrome storage
     chrome.storage.sync.set({ arrowColor: color }, () => {
-      alert("Arrow color saved!");
+        console.log("Arrow color saved!"); // Optional: Use console.log for debugging
     });
-  });
-  
+});
